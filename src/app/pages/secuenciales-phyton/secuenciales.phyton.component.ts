8db35c9a19c54e8dfd7327eac957774f43ad523a
leonardo2710 <lugo.leo1218@gmail.com>
2022-09-20 09:48:34 -0500

    se ha completado las ventanas

--- src/app/pages/secuenciales-phyton/secuenciales.phyton.component.ts
+++ src/app/pages/secuenciales-phyton/secuenciales.phyton.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-phyton',
   templateUrl: './secuenciales.phyton.component.html',
-  styleUrls: ['./secuenciales.phyton.component.css']
+  styleUrls: ['./secuenciales.phyton.component.css'] 
 })
 export class SecuencialesPhytonComponent implements OnInit {
 
