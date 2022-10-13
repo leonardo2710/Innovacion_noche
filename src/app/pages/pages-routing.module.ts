ddf6bf299c9a08a075fc157f039cdd0f9681285d
Duban Tovar <duvan9430@gmail.com>
2022-10-13 03:34:33 -0500

    fix:merge

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -11,32 +11,18 @@ import { AuthGuard } from '../services/auth.guard';
 const routes:Routes=[
   {path:'dashboard', component:PagesComponent,canActivate: [AuthGuard],
     children:[
-<<<<<<< HEAD
-=======
       {path:'', component:DashboardComponent, data:{titulo:'BIENVENIDOS SECCIÓN - TEMAS'}},
     ]
   },
   {path:'dashboard', component:PagesComponent,canActivate: [AuthGuard],
     children:[
->>>>>>> dev-leo
 
       {path:'', component:DashboardComponent, data:{titulo:' Curso Phyton'}}, 
       {path:'secuenciales-phyton', component:NavGeneralComponent,data:{titulo:'Secuenciales Python'}},
       {path:'condicionales-phyton', component:NavGeneralComponent, data:{titulo:'Condicionales Python'}},
       {path:'swicht-phyton', component:NavGeneralComponent, data:{titulo:'Swicht Python'}},
       {path:'for-phyton', component:NavGeneralComponent, data:{titulo:'For Python'}},
-<<<<<<< HEAD
-      {path:'while-phyton', component:NavGeneralComponent, data:{titulo:'While Python'}},
-
-      {path:'', component:DashboardComponent, data:{titulo:' Curso Phyton'},canActivate: [AuthGuard]}, 
-      {path:'secuenciales-phyton', component:NavGeneralComponent,data:{titulo:'Secuenciales Python'},canActivate: [AuthGuard]},
-      {path:'condicionales-phyton', component:NavGeneralComponent, data:{titulo:'Condicionales Python'},canActivate: [AuthGuard]},
-      {path:'swicht-phyton', component:NavGeneralComponent, data:{titulo:'Swicht Python'},canActivate: [AuthGuard]},
-      {path:'for-phyton', component:NavGeneralComponent, data:{titulo:'For Python'},canActivate: [AuthGuard]},
-      {path:'while-phyton', component:NavGeneralComponent, data:{titulo:'While Python'},canActivate: [AuthGuard]}
-=======
       {path:'while-phyton', component:NavGeneralComponent, data:{titulo:'While Python'}}
->>>>>>> dev-leo
 
     ]
   },
@@ -48,18 +34,7 @@ const routes:Routes=[
       {path:'condicionales-php', component:NavGeneralComponent, data:{titulo:'Condicionales PHP'}},
       {path:'swicht-php', component:NavGeneralComponent, data:{titulo:'Swicht PHP'}},
       {path:'for-php', component:NavGeneralComponent, data:{titulo:'For PHP'}},
-<<<<<<< HEAD
-      {path:'while-php', component:NavGeneralComponent, data:{titulo:'While PHP'}},
-
-      {path:'', component:DashboardComponent, data:{titulo:'Curso PhP'},canActivate: [AuthGuard]}, 
-      {path:'secuenciales-php', component:NavGeneralComponent,data:{titulo:'Secuenciales PHP'},canActivate: [AuthGuard]},
-      {path:'condicionales-php', component:NavGeneralComponent, data:{titulo:'Condicionales PHP'},canActivate: [AuthGuard]},
-      {path:'swicht-php', component:NavGeneralComponent, data:{titulo:'Swicht PHP'},canActivate: [AuthGuard]},
-      {path:'for-php', component:NavGeneralComponent, data:{titulo:'For PHP'},canActivate: [AuthGuard]},
-      {path:'while-php', component:NavGeneralComponent, data:{titulo:'While PHP'},canActivate: [AuthGuard]}
-=======
       {path:'while-php', component:NavGeneralComponent, data:{titulo:'While PHP'}}
->>>>>>> dev-leo
 
     ]
   },
@@ -71,18 +46,7 @@ const routes:Routes=[
       {path:'condicionales-javascript', component:NavGeneralComponent, data:{titulo:'Condicionales Javascript'}},
       {path:'swicht-javascript', component:NavGeneralComponent, data:{titulo:'Swicht Javascript'}},
       {path:'for-javascript', component:NavGeneralComponent, data:{titulo:'For Javascript'}},
-<<<<<<< HEAD
-      {path:'while-javascript', component:NavGeneralComponent, data:{titulo:'While Javascript'}},
-
-      {path:'', component:DashboardComponent, data:{titulo:'Curso Javascript'},canActivate: [AuthGuard]}, 
-      {path:'secuenciales-javascript', component:NavGeneralComponent,data:{titulo:'Secuenciales Javascript'},canActivate: [AuthGuard]},
-      {path:'condicionales-javascript', component:NavGeneralComponent, data:{titulo:'Condicionales Javascript'},canActivate: [AuthGuard]},
-      {path:'swicht-javascript', component:NavGeneralComponent, data:{titulo:'Swicht Javascript'},canActivate: [AuthGuard]},
-      {path:'for-javascript', component:NavGeneralComponent, data:{titulo:'For Javascript'},canActivate: [AuthGuard]},
-      {path:'while-javascript', component:NavGeneralComponent, data:{titulo:'While Javascript'},canActivate: [AuthGuard]}
-=======
       {path:'while-javascript', component:NavGeneralComponent, data:{titulo:'While Javascript'}}
->>>>>>> dev-leo
 
     ]
   },
@@ -95,18 +59,7 @@ const routes:Routes=[
       {path:'condicionales-java', component:NavGeneralComponent, data:{titulo:'Condicionales Java'}},
       {path:'swicht-java', component:NavGeneralComponent, data:{titulo:'Swicht Java'}},
       {path:'for-java', component:NavGeneralComponent, data:{titulo:'For Java'}},
-<<<<<<< HEAD
-      {path:'while-java', component:NavGeneralComponent, data:{titulo:'While Java'}},
-    
-      {path:'', component:DashboardComponent, data:{titulo:'Curso Java'},canActivate: [AuthGuard]}, 
-      {path:'secuenciales-java', component:NavGeneralComponent,data:{titulo:'Secuenciales Java'},canActivate: [AuthGuard]},
-      {path:'condicionales-java', component:NavGeneralComponent, data:{titulo:'Condicionales Java'},canActivate: [AuthGuard]},
-      {path:'swicht-java', component:NavGeneralComponent, data:{titulo:'Swicht Java'},canActivate: [AuthGuard]},
-      {path:'for-java', component:NavGeneralComponent, data:{titulo:'For Java'},canActivate: [AuthGuard]},
-      {path:'while-java', component:NavGeneralComponent, data:{titulo:'While Java'},canActivate: [AuthGuard]}
-=======
       {path:'while-java', component:NavGeneralComponent, data:{titulo:'While Java'}}
->>>>>>> dev-leo
 
     ]
   },
@@ -124,15 +77,7 @@ const routes:Routes=[
 
       {path:'swicht-pseint', component:NavGeneralComponent, data:{titulo:'Swicht pseint'}},
       {path:'for-pseint', component:NavGeneralComponent, data:{titulo:'For pseint'}},
-<<<<<<< HEAD
-      {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint'}},
-      {path:'condicionales-pseint', component:NavGeneralComponent, data:{titulo:'Condicionales pseint'},canActivate: [AuthGuard]},
-      {path:'swicht-pseint', component:NavGeneralComponent, data:{titulo:'Swicht pseint'},canActivate: [AuthGuard]},
-      {path:'for-pseint', component:NavGeneralComponent, data:{titulo:'For pseint'},canActivate: [AuthGuard]},
-      {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint'},canActivate: [AuthGuard]}
-=======
       {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint'}}
->>>>>>> dev-leo
 
     ]
   },
@@ -144,18 +89,7 @@ const routes:Routes=[
       {path:'condicionales-visual', component:NavGeneralComponent, data:{titulo:'Condicionales Visual Basic'}},
       {path:'swicht-visual', component:NavGeneralComponent, data:{titulo:'Swicht Visual Basic'}},
       {path:'for-visual', component:NavGeneralComponent, data:{titulo:'For Visual Basic'}},
-<<<<<<< HEAD
-      {path:'while-visual', component:NavGeneralComponent, data:{titulo:'While Visual Basic'}},
-
-      {path:'', component:DashboardComponent, data:{titulo:'Curso Visual Basic'},canActivate: [AuthGuard]}, 
-      {path:'secuenciales-visual', component:NavGeneralComponent,data:{titulo:'Secuenciales Visual Basic'},canActivate: [AuthGuard]},
-      {path:'condicionales-visual', component:NavGeneralComponent, data:{titulo:'Condicionales Visual Basic'},canActivate: [AuthGuard]},
-      {path:'swicht-visual', component:NavGeneralComponent, data:{titulo:'Swicht Visual Basic'},canActivate: [AuthGuard]},
-      {path:'for-visual', component:NavGeneralComponent, data:{titulo:'For Visual Basic'},canActivate: [AuthGuard]},
-      {path:'while-visual', component:NavGeneralComponent, data:{titulo:'While Visual Basic'},canActivate: [AuthGuard]}
-=======
       {path:'while-visual', component:NavGeneralComponent, data:{titulo:'While Visual Basic'}}
->>>>>>> dev-leo
 
     ]
   },
