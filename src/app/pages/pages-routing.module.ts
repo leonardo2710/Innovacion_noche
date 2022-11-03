b662f9718e215eaa3dfdffa78fce7306f0cdd5fe
dacota20 <64114045+dacota20@users.noreply.github.com>
2022-11-02 22:28:42 -0500

    comentario

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -74,7 +74,6 @@ const routes:Routes=[
       {path:'secuenciales-ejercicio4', component:NavGeneralComponent,data:{titulo:'Ejercicio 4 pseint'}},
       {path:'secuenciales-ejercicio5', component:NavGeneralComponent,data:{titulo:'Ejercicio 5 pseint'}},
       {path:'condicionales-pseint', component:NavGeneralComponent, data:{titulo:'Condicionales pseint'}},
-
       {path:'swicht-pseint', component:NavGeneralComponent, data:{titulo:'Swicht pseint'}},
       {path:'for-pseint', component:NavGeneralComponent, data:{titulo:'For pseint'}},
       {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint'}}
