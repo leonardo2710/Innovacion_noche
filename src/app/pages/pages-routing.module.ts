1c21098efdce44f072ade6e3fd4387d3230c4702
Duban <duvan9430@gmail.com>
2022-11-05 20:08:14 -0500

    fix:ajustando manu con tema traido

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -20,11 +20,11 @@ const routes:Routes=[
     children:[
 
       {path:'', component:DashboardComponent, data:{titulo:' Curso Phyton'}},
-      {path:'secuenciales-phyton', component:NavGeneralComponent,data:{titulo:'Secuenciales Python'}},
-      {path:'condicionales-phyton', component:NavGeneralComponent, data:{titulo:'Condicionales Python'}},
+      {path:'secuenciales-phyton', component:NavGeneralComponent,data:{titulo:'Secuenciales Python', id:11}},
+      {path:'condicionales-phyton', component:NavGeneralComponent, data:{titulo:'Condicionales Python', id:11}},
       {path:'swicht-phyton', component:NavGeneralComponent, data:{titulo:'Swicht Python'}},
-      {path:'for-phyton', component:NavGeneralComponent, data:{titulo:'For Python'}},
-      {path:'while-phyton', component:NavGeneralComponent, data:{titulo:'While Python'}}
+      {path:'for-phyton', component:NavGeneralComponent, data:{titulo:'For Python', id:11}},
+      {path:'while-phyton', component:NavGeneralComponent, data:{titulo:'While Python', id:11}}
 
     ]
   },
@@ -32,11 +32,11 @@ const routes:Routes=[
     children:[
 
       {path:'', component:DashboardComponent, data:{titulo:'Curso PhP'}},
-      {path:'secuenciales-php', component:NavGeneralComponent,data:{titulo:'Secuenciales PHP'}},
-      {path:'condicionales-php', component:NavGeneralComponent, data:{titulo:'Condicionales PHP'}},
+      {path:'secuenciales-php', component:NavGeneralComponent,data:{titulo:'Secuenciales PHP', id:10}},
+      {path:'condicionales-php', component:NavGeneralComponent, data:{titulo:'Condicionales PHP', id:10}},
       {path:'swicht-php', component:NavGeneralComponent, data:{titulo:'Swicht PHP'}},
-      {path:'for-php', component:NavGeneralComponent, data:{titulo:'For PHP'}},
-      {path:'while-php', component:NavGeneralComponent, data:{titulo:'While PHP'}}
+      {path:'for-php', component:NavGeneralComponent, data:{titulo:'For PHP', id:10}},
+      {path:'while-php', component:NavGeneralComponent, data:{titulo:'While PHP', id:10}}
 
     ]
   },
@@ -44,11 +44,11 @@ const routes:Routes=[
     children:[
 
       {path:'', component:DashboardComponent, data:{titulo:'Curso Javascript'}},
-      {path:'secuenciales-javascript', component:NavGeneralComponent,data:{titulo:'Secuenciales Javascript'}},
-      {path:'condicionales-javascript', component:NavGeneralComponent, data:{titulo:'Condicionales Javascript'}},
+      {path:'secuenciales-javascript', component:NavGeneralComponent,data:{titulo:'Secuenciales Javascript', id:5}},
+      {path:'condicionales-javascript', component:NavGeneralComponent, data:{titulo:'Condicionales Javascript', id:6}},
       {path:'swicht-javascript', component:NavGeneralComponent, data:{titulo:'Swicht Javascript'}},
-      {path:'for-javascript', component:NavGeneralComponent, data:{titulo:'For Javascript'}},
-      {path:'while-javascript', component:NavGeneralComponent, data:{titulo:'While Javascript'}}
+      {path:'for-javascript', component:NavGeneralComponent, data:{titulo:'For Javascript', id:7}},
+      {path:'while-javascript', component:NavGeneralComponent, data:{titulo:'While Javascript', id:8}}
 
     ]
   },
@@ -59,7 +59,7 @@ const routes:Routes=[
       {path:'', component:DashboardComponent, data:{titulo:'Curso Java'}},
       {path:'secuenciales-java', component:NavGeneralComponent,data:{titulo:'Secuenciales Java', id:5}},
       {path:'condicionales-java', component:NavGeneralComponent, data:{titulo:'Condicionales Java', id:6}},
-      {path:'swicht-java', component:NavGeneralComponent, data:{titulo:'Swicht Java'}},
+      {path:'swicht-java', component:NavGeneralComponent, data:{titulo:'Swicht Java',id:9}},
       {path:'for-java', component:NavGeneralComponent, data:{titulo:'For Java', id:7}},
       {path:'while-java', component:NavGeneralComponent, data:{titulo:'While Java', id:8}}
 
@@ -69,26 +69,21 @@ const routes:Routes=[
     children:[
       {path:'', component:DashboardComponent, data:{titulo:'Curso Pseint'}},
       {path:'secuenciales-pseint', component:NavGeneralComponent,data:{titulo:'Secuenciales pseint', id:1}},
-      {path:'secuenciales-ejercicio1', component:NavGeneralComponent,data:{titulo:'Ejercicio 1 pseint'}},
-      {path:'secuenciales-ejercicio2', component:NavGeneralComponent,data:{titulo:'Ejercicio 2 pseint'}},
-      {path:'secuenciales-ejercicio3', component:NavGeneralComponent,data:{titulo:'Ejercicio 3 pseint'}},
-      {path:'secuenciales-ejercicio4', component:NavGeneralComponent,data:{titulo:'Ejercicio 4 pseint'}},
-      {path:'secuenciales-ejercicio5', component:NavGeneralComponent,data:{titulo:'Ejercicio 5 pseint'}},
-      {path:'condicionales-pseint', component:NavGeneralComponent, data:{titulo:'Condicionales pseint'}},
+      {path:'condicionales-pseint', component:NavGeneralComponent, data:{titulo:'Condicionales pseint', id:2}},
       {path:'swicht-pseint', component:NavGeneralComponent, data:{titulo:'Swicht pseint'}},
-      {path:'for-pseint', component:NavGeneralComponent, data:{titulo:'For pseint'}},
-      {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint'}}
+      {path:'for-pseint', component:NavGeneralComponent, data:{titulo:'For pseint', id:3}},
+      {path:'while-pseint', component:NavGeneralComponent, data:{titulo:'While pseint', id:4}}
     ]
   },
   {path:'dashboard', component:PagesComponent,
     children:[
 
       {path:'', component:DashboardComponent, data:{titulo:'Curso Visual Basic'}},
-      {path:'secuenciales-visual', component:NavGeneralComponent,data:{titulo:'Secuenciales Visual Basic'}},
-      {path:'condicionales-visual', component:NavGeneralComponent, data:{titulo:'Condicionales Visual Basic'}},
+      {path:'secuenciales-visual', component:NavGeneralComponent,data:{titulo:'Secuenciales Visual Basic', id:1}},
+      {path:'condicionales-visual', component:NavGeneralComponent, data:{titulo:'Condicionales Visual Basic', id:2}},
       {path:'swicht-visual', component:NavGeneralComponent, data:{titulo:'Swicht Visual Basic'}},
-      {path:'for-visual', component:NavGeneralComponent, data:{titulo:'For Visual Basic'}},
-      {path:'while-visual', component:NavGeneralComponent, data:{titulo:'While Visual Basic'}}
+      {path:'for-visual', component:NavGeneralComponent, data:{titulo:'For Visual Basic', id:3}},
+      {path:'while-visual', component:NavGeneralComponent, data:{titulo:'While Visual Basic', id:4}}
 
     ]
   },
